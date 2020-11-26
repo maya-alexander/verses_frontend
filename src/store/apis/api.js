1e2951e5b36f97c723d1726705580030722a5b66
mayalexandera <maya.blevins@gmail.com>
2020-11-26 17:19:31 -0500

    remove allow-origin header

--- src/store/apis/api.js
+++ src/store/apis/api.js
@@ -5,8 +5,7 @@ const token = localStorage.getItem("token")
 export default axios.create({
   baseURL: `https://verses-backend.herokuapp.com/api/v1`,
   headers: { 
-    'Authorization': `Bearer ${token}`,
-    'Access-Control-Allow-Origin': 'https://verses.netlify.app'
+    'Authorization': `Bearer ${token}`
   }
 });
   
