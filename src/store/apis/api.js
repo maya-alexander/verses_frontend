c408754222f40f0a65ffb2faa3235fe442a7edb1
mayalexandera <maya.blevins@gmail.com>
2020-11-26 16:42:44 -0500

    correct origin url

--- src/store/apis/api.js
+++ src/store/apis/api.js
@@ -6,7 +6,7 @@ export default axios.create({
   baseURL: `https://verses-backend.herokuapp.com/api/v1`,
   headers: { 
     'Authorization': `Bearer ${token}`,
-    'Access-Control-Allow-Origin': 'https://verses.neflify.app'
+    'Access-Control-Allow-Origin': 'https://verses.netlify.app'
   }
 });
   
